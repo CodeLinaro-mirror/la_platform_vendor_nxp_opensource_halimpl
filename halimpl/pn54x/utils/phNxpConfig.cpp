aae89363ff525f9d4e56bb353c46408fbf8067d2
Gaurav Singhal <gsinghal@codeaurora.org>
2018-04-25 12:04:45 +0530

    NFC: Add dynamic config support for SDA660
    Dynamic configuration support is added for SDA660
    platform to pick Nfc configuration file at
    runtime based on hw platform.
    
    Change-Id: I2c9ee73fae931ffb781efadf086ce7ce34ae9748

--- halimpl/pn54x/utils/phNxpConfig.cpp
+++ halimpl/pn54x/utils/phNxpConfig.cpp
@@ -127,6 +127,7 @@ typedef enum
   TARGET_MSM8940                       = 313, /**< 8940 target */
   TARGET_SDM660                        = 317, /**< SDM660 target */
   TARGET_SDM630                        = 318, /**< SDM630 target */
+  TARGET_SDA660                        = 324, /**< SDA660 target */
   TARGET_DEFAULT                       = TARGET_GENERIC, /**< new targets */
   TARGET_INVALID                       = 0xFF
 } TARGETTYPE;
@@ -354,6 +355,7 @@ int CNfcConfig::getconfiguration_id (char * config_file)
             strlcpy(config_file, config_name_qrd1, MAX_DATA_CONFIG_PATH_LEN);
             config_id = QRD_TYPE_1;
             break;
+        case TARGET_SDA660:
         case TARGET_SDM660:
         case TARGET_SDM630:
         case TARGET_MSM8998:
@@ -380,6 +382,7 @@ int CNfcConfig::getconfiguration_id (char * config_file)
         case TARGET_GENERIC:
             config_id = CONFIG_GENERIC;
             break;
+        case TARGET_SDA660:
         case TARGET_SDM660:
         case TARGET_SDM630:
         case TARGET_MSM8998:
