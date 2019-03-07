6a78a78f12bdd7f972c33e75f47424372bd75aea
Gaurav Singhal <gsinghal@codeaurora.org>
2019-03-07 13:02:56 +0530

    NFC: Fix deadlock and power leakage
    Leakage observed due to deadlock as VEN gpio is still HIGH,
    issue reproducible if we reboot phone with NFC disable state.
    
    Thread 1 acquires lock and call HAL close which is a blocking
    function, it will wait for client thread to be finished.
    Thread 2 wants to send event for client thread to stop
    but need to acquire same lock and causing deadlock.
    
    Remove lock before HAL close as it's blocking function and ensure
    client thread stopped properly using pthread_join.
    
    Change-Id: Ia2b19465465e9d1076acab4dd025de4abc1976bc

--- SN100x/src/adaptation/NfcAdaptation.cc
+++ SN100x/src/adaptation/NfcAdaptation.cc
@@ -68,7 +68,9 @@ ThreadMutex NfcAdaptation::sLock;
 tHAL_NFC_CBACK* NfcAdaptation::mHalCallback = NULL;
 tHAL_NFC_DATA_CBACK* NfcAdaptation::mHalDataCallback = NULL;
 ThreadCondVar NfcAdaptation::mHalOpenCompletedEvent;
+#if (NXP_EXTNS == FALSE)
 ThreadCondVar NfcAdaptation::mHalCloseCompletedEvent;
+#endif
 sp<INfc> NfcAdaptation::mHal;
 sp<INfcV1_1> NfcAdaptation::mHal_1_1;
 INfcClientCallback* NfcAdaptation::mCallback;
@@ -831,10 +833,8 @@ void NfcAdaptation::DownloadFirmware() {
   HalOpen(HalDownloadFirmwareCallback, HalDownloadFirmwareDataCallback);
   mHalOpenCompletedEvent.wait();
 
-  mHalCloseCompletedEvent.lock();
   DLOG_IF(INFO, nfc_debug_enabled) << StringPrintf("%s: try close HAL", func);
   HalClose();
-  mHalCloseCompletedEvent.wait();
 
   HalTerminate();
   DLOG_IF(INFO, nfc_debug_enabled) << StringPrintf("%s: exit", func);
@@ -865,7 +865,9 @@ void NfcAdaptation::HalDownloadFirmwareCallback(nfc_event_t event,
     case HAL_NFC_CLOSE_CPLT_EVT: {
       DLOG_IF(INFO, nfc_debug_enabled)
           << StringPrintf("%s: HAL_NFC_CLOSE_CPLT_EVT", func);
-      mHalCloseCompletedEvent.signal();
+#if (NXP_EXTNS == FALSE)
+        mHalCloseCompletedEvent.signal();
+#endif
       break;
     }
   }
