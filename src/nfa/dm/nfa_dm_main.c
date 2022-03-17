c2fc6c847ccd5e1a59e15f2e9612c6392988b369
Alisher Alikhodjaev <alisher@google.com>
2022-03-17 15:39:20 -0700

    Out of Bounds Read in nfa_dm_check_set_config
    Bug: 221216105
    Test: build ok
    Change-Id: I1930de8531f6c15e6be400a7b1ab3e7cf86b4229
    (cherry picked from commit 88c5c267e889699c71412022e3fcb03d20100e99)
    Merged-In: I1930de8531f6c15e6be400a7b1ab3e7cf86b4229
    (cherry picked from commit 9a8a317382782b03cf7d1fc39b48838089f0ef67)

--- src/nfa/dm/nfa_dm_main.c
+++ src/nfa/dm/nfa_dm_main.c
@@ -21,6 +21,11 @@
  *  This is the main implementation file for the NFA device manager.
  *
  ******************************************************************************/
+#include <string>
+
+#include <android-base/stringprintf.h>
+#include <base/logging.h>
+#include <log/log.h>
 
 #include <string.h>
 #include "nfa_api.h"
@@ -235,7 +240,13 @@ tNFA_STATUS nfa_dm_check_set_config(uint8_t tlv_list_len, uint8_t* p_tlv_list,
     type = *(p_tlv_list + xx);
     len = *(p_tlv_list + xx + 1);
     p_value = p_tlv_list + xx + 2;
-    p_cur_len = NULL;
+    p_cur_len = nullptr;
+    if (len > (tlv_list_len - xx - 2)) {
+      LOG(ERROR) << StringPrintf("error: invalid TLV length: t:0x%x, l:%d",
+                                 type, len);
+      android_errorWriteLog(0x534e4554, "221216105");
+      return NFA_STATUS_FAILED;
+    }
 
     switch (type) {
       /*
