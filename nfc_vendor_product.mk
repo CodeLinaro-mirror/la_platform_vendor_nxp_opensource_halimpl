f2da746d29235382c4452d1a0a3f794d367567fb
Khageswararao Rao B <quic_kraob@quicinc.com>
2022-11-12 23:34:40 +0530

    NFC: halimpl: enable compilation for pineapple
    Added compilation flag for pineapple target.
    
    Change-Id: Ic1f75482f85b2026823de723fdeb984f5fd0295a

--- nfc_vendor_product.mk
+++ nfc_vendor_product.mk
@@ -1,5 +1,5 @@
 # Enable build support for NFC open source vendor modules
-ifeq ($(call is-board-platform-in-list, kalama bengal),true)
+ifeq ($(call is-board-platform-in-list, pineapple),true)
 TARGET_USES_NQ_NFC := true
 endif
 
