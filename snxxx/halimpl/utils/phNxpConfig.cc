1cb13fb03ed5cfa9748a976a89a3f28d7cd942c0
Khageswararao Rao B <quic_kraob@quicinc.com>
2022-11-13 11:53:39 +0530

    NFC: Add dynamic config support for pineapple
    Dynamic configuration support is added in HAL layer
    for Pineapple platform to pick Nfc configuration file
    at runtime based on hw  platform.
    
    Change-Id: Icf9554c803eae46962567dbc465dfa9c51097209

--- snxxx/halimpl/utils/phNxpConfig.cc
+++ snxxx/halimpl/utils/phNxpConfig.cc
@@ -142,9 +142,8 @@ typedef enum
 typedef enum
 {
   TARGET_GENERIC                       = 0x00,/**< new targets */
-  TARGET_SM_DIVAR                      = 518, /**< SM_DIVAR target */
-  TARGET_SM_KAILUA                     = 519, /**< SM_KAILUA target */
-  TARGET_SMP_KAILUA                    = 536, /**< SMP_KAILUA target */
+  TARGET_SM_LANAI                      = 557, /**< SM_LANAI target */
+  TARGET_SMP_LANAI                     = 577, /**< SMP_LANAI target */
   TARGET_DEFAULT                       = TARGET_GENERIC, /**< new targets */
   TARGET_INVALID                       = 0xFF
 } TARGETTYPE;
@@ -328,7 +327,7 @@ bool secure_zone_support(void)
         return DEFAULT_CONFIG;
     }
     msm_id = atoi(soc_info);
-    if ((msm_id == TARGET_SM_KAILUA) || (msm_id == TARGET_SMP_KAILUA))
+    if ((msm_id == TARGET_SM_LANAI) || (msm_id == TARGET_SMP_LANAI))
 	return true;
     else
 	return false;
@@ -397,21 +396,11 @@ int CNfcConfig::getconfiguration_id (char * config_file)
         case TARGET_GENERIC:
             config_id = CONFIG_GENERIC;
             break;
-        case TARGET_SM_DIVAR:
-            config_id = GENERIC_38_4_TYPE_SN1xx;
-            strlcpy(config_file, config_name_qrd_SN100_38_4MHZ, MAX_DATA_CONFIG_PATH_LEN);
-            break;
-        case TARGET_SM_KAILUA:
-        case TARGET_SMP_KAILUA:
-            if (!strncmp(nq_chip_info.nq_chipid, SN220_CHIP_ID, PROPERTY_VALUE_MAX)) {
-                // SN220
-                config_id = GENERIC_38_4_TYPE_SN220;
-                strlcpy(config_file, config_name_SN220_38_4MHZ, MAX_DATA_CONFIG_PATH_LEN);
-            } else {
-                // SN110 or SN100
-                config_id = GENERIC_38_4_TYPE_SN1xx;
-                strlcpy(config_file, config_name_qrd_SN100_38_4MHZ, MAX_DATA_CONFIG_PATH_LEN);
-            }
+        case TARGET_SM_LANAI:
+        case TARGET_SMP_LANAI:
+            // SN220 V1 and V3
+            config_id = GENERIC_38_4_TYPE_SN220;
+            strlcpy(config_file, config_name_SN220_38_4MHZ, MAX_DATA_CONFIG_PATH_LEN);
             break;
         default:
             config_id = QRD_TYPE_DEFAULT;
@@ -426,21 +415,11 @@ int CNfcConfig::getconfiguration_id (char * config_file)
         case TARGET_GENERIC:
             config_id = CONFIG_GENERIC;
             break;
-        case TARGET_SM_DIVAR:
-            config_id = GENERIC_38_4_TYPE_SN1xx;
-            strlcpy(config_file, config_name_mtp_SN100_38_4MHZ, MAX_DATA_CONFIG_PATH_LEN);
-            break;
-        case TARGET_SM_KAILUA:
-        case TARGET_SMP_KAILUA:
-            if (!strncmp(nq_chip_info.nq_chipid, SN220_CHIP_ID, PROPERTY_VALUE_MAX)) {
-                // SN220
-                config_id = GENERIC_38_4_TYPE_SN220;
-                strlcpy(config_file, config_name_SN220_38_4MHZ, MAX_DATA_CONFIG_PATH_LEN);
-            } else {
-                // SN110 or SN100
-                config_id = GENERIC_38_4_TYPE_SN1xx;
-                strlcpy(config_file, config_name_mtp_SN100_38_4MHZ, MAX_DATA_CONFIG_PATH_LEN);
-            }
+        case TARGET_SM_LANAI:
+        case TARGET_SMP_LANAI:
+            // SN220 V1 and V3
+            config_id = GENERIC_38_4_TYPE_SN220;
+            strlcpy(config_file, config_name_SN220_38_4MHZ, MAX_DATA_CONFIG_PATH_LEN);
             break;
         default:
             config_id = MTP_TYPE_DEFAULT;
