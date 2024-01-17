478364c3b3bef819a50e4a4524b1f4de4572d372
Amruth Naga <quic_amrunaga@quicinc.com>
2024-01-17 11:54:42 +0530

    NFC:halimpl: Enable secure peripheral feature
    Enabled secure peripheral feature for sun product
    
    Change-Id: I7229e4e2c959c3892204261ccf83c9aef96a8a98

--- nfc_vendor_product.mk
+++ nfc_vendor_product.mk
@@ -20,10 +20,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += $(NQ_VENDOR_NFC)
 endif
 
-ifeq ($(call is-board-platform-in-list, pineapple),true)
+ifeq ($(call is-board-platform-in-list, sun),true)
 TARGET_ENABLE_PERIPHERAL_CONTROL := true
 ifeq ($(TARGET_ENABLE_PERIPHERAL_CONTROL), true)
-    $(call soong_config_set,nfc,board_secure_peripheral_framework,pineapple)
+    $(call soong_config_set,nfc,board_secure_peripheral_framework,sun)
 endif
 endif
 
