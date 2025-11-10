f326f6717ef0d1a9363918e8f4c299d13a62f774
Amruth Naga <amrunaga@qti.qualcomm.com>
2025-11-10 11:17:06 +0530

    [NFC][Halimpl]: Add dynamic config support to bengal and khaje
    - Added Dynamic config support to bengal and khaje targets
    
    Change-Id: I8375763e522e4bbc8ba0fd3437b60d5dc57a8c04

--- nfc_vendor_product.mk
+++ nfc_vendor_product.mk
@@ -1,5 +1,5 @@
 # Enable build support for NFC open source vendor modules
-ifeq ($(call is-board-platform-in-list, sun canoe),true)
+ifeq ($(call is-board-platform-in-list, sun canoe bengal),true)
 TARGET_USES_NQ_NFC := true
 endif
 
